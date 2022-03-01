2cd9e2c0a4fe3819ac5bcc71cfb15eacd6ff65b0
lucsimao <lucasimao64@gmail.com>
2022-03-01 20:51:45 -0300

    feat: ensure show correct navigation items

---
+++ src/components/header/navigation-items.tsx
@@ -0,0 +1,27 @@
+import Item from './item';
+import React from 'react';
+import styled from 'styled-components';
+
+const Items = styled.ul`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  align-content: center;
+  margin-top: 1em;
+  margin-bottom: 1em;
+  color: white;
+  user-select: none;
+  cursor: default;
+`;
+
+export default function NavigationItems() {
+  return (
+    <Items data-testid="navigation-items">
+      <Item href="/browse" content="Início" active={true} />
+      <Item href="/browse/genre/83" content="Séries" />
+      <Item href="/browse/genre/34399" content="Filmes" />
+      <Item href="/latest" content="Bombando" />
+      <Item href="/browse/my-list" content="Minha lista" />
+    </Items>
+  );
+}
