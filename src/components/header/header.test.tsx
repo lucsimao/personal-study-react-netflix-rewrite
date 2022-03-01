743685cfdf66b011f9ea6ede604429ea75871cb4
lucsimao <lucasimao64@gmail.com>
2022-03-01 12:42:37 -0300

    test: ensure show Netflix label

---
+++ src/components/header/header.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+
+import Header from './header';
+import React from 'react';
+
+describe('Header', () => {
+  describe('when page is loaded', () => {
+    it('should show Netflix label ', () => {
+      render(<Header />);
+
+      const result = screen.getByText('Netflix');
+
+      expect(result).toBeInTheDocument();
+    });
+  });
+});
