d36d9f367d6a7b3c6b3bba22a6430a2b772f8a29
lucsimao <lucasimao64@gmail.com>
2022-03-01 17:05:25 -0300

    feat: create nav container

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  display: flex;
+  align-content: center;
+  background-color: black;
+  height: 41px;
+  padding: 0px 4% 0px 4%;
+`;
+
 const Logo = styled.a`
   cursor: pointer;
   content: url('https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg');
@@ -10,7 +18,9 @@ const Logo = styled.a`
 export default function Header() {
   return (
     <>
-      <Logo data-testid="netflix-logo" href="/" />
+      <Container>
+        <Logo data-testid="netflix-logo" href="/" />
+      </Container>
     </>
   );
 }
