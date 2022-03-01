b971e63bd03ab2c16eaa1da86ae1682f8b4ed5c8
lucsimao <lucasimao64@gmail.com>
2022-03-01 20:52:44 -0300

    feat: update header with navigation item

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,20 @@
+import NavigationItems from './navigation-items';
 import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
-  align-content: center;
+  align-items: center;
   background-color: black;
-  height: 41px;
-  padding: 0px 4% 0px 4%;
+  height: 68px;
+  padding: 0px 60px 0px 60px;
 `;
 
 const Logo = styled.a`
   cursor: pointer;
   content: url('https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg');
-  height: 2em;
+  height: 1.563em;
+  margin-right: 25px;
 `;
 
 export default function Header() {
@@ -20,6 +22,7 @@ export default function Header() {
     <>
       <Container>
         <Logo data-testid="netflix-logo" href="/" />
+        <NavigationItems />
       </Container>
     </>
   );
