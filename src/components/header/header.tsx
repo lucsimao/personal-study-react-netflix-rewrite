f65351757ddae2ec55bb57e12af0bc1d5895e5d9
lucsimao <lucasimao64@gmail.com>
2022-03-06 02:07:07 -0300

    refactor: move styled components to separated folder

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,22 +1,8 @@
+import { Container, Logo } from './styled';
+
 import NavigationItems from './navigation-items';
 import OptionsMenu from './options-menu';
 import React from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  background-color: black;
-  height: 68px;
-  padding: 0px 60px 0px 60px;
-`;
-
-const Logo = styled.a`
-  cursor: pointer;
-  content: url('https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg');
-  height: 1.563em;
-  margin-right: 25px;
-`;
 
 export default function Header() {
   return (
---
+++ src/components/header/styled.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: black;
+  height: 68px;
+  padding: 0px 60px 0px 60px;
+`;
+
+export const Logo = styled.a`
+  cursor: pointer;
+  content: url('https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg');
+  height: 1.563em;
+  margin-right: 25px;
+`;
