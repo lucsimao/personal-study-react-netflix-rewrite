8e0e3fea660a8569635b6fa38a1588bf87036196
lucsimao <lucasimao64@gmail.com>
2022-03-06 01:52:49 -0300

    feat: update item style

--- src/components/header/item.tsx
+++ src/components/header/item.tsx
@@ -4,9 +4,7 @@ import styled from 'styled-components';
 const Container = styled.li`
   list-style-type: none;
   margin-left: 20px;
-  font-family: sans-serif;
-  font-size: 1.2;
-  font-size: 14px;
+  font-size: 0.88em;
   white-space: nowrap;
 `;
 
@@ -14,10 +12,11 @@ const StyledItem = styled.a`
   color: white;
   text-decoration: none;
   font-family: 'Netflix Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: 300;
 `;
 
 const SelectedItem = styled(StyledItem)`
-  font-weight: 700;
+  font-weight: 450;
 `;
 
 export default function Item(props: {
